e9cd5eb89dedd91000ea4acd98053a7e1ef32823
Tomasz776 <159617119+Tomasz776@users.noreply.github.com>
2024-02-10 11:00:01 +0100

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+meme_dict = {
+            "CRINGE": "Coś wyjątkowo dziwnego lub zawstydzającego",
+            "LOL": "Częsta reakcja na coś zabawnego",
+            }
+word = input("Wpisz słowo, którego nie rozumiesz (używaj wielkich liter!): ")
+print(meme_dict("CRINGE")
+if word in meme_dict.keys():
+    print(meme_dict(0))
+else:
+   print("przepraszamy takiego słowa jeszcze nie dodaliśmy")

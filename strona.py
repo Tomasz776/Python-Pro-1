172b9152ee3dbfe91082d80183eba8d0f17f5f79
Tomek <159617119+Tomasz776@users.noreply.github.com>
2024-04-13 10:00:44 +0200

    Create strona.py

---
+++ strona.py
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>przydatna informacja!</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Zależność od sieci społecznościowej</h1>
+    <p>Media społecznościowe skracają czas, jaki spędzamy w realnym świecie. Uwarunkowa to także nasze umysły do ciągłych aktualizacji wiadomości i postów, co na dłuższą metę może stać się stresujące.</p>
+    <img src=" https://media3.giphy.com/media/26BGGxgsEh42ABHFe/giphy.gif?cid=ecf05e47fewnmm2h6vijgpd1768vz8zxo441mpec89684gtw&rid=giphy.gif&ct=g" alt="Obraz 1">
+    <h2>jak możemy się temu preciwstawić?</h2>
+             <ul>
+                   <li> Możemy ustawić limity czasu spędzanego online/na naszych urządzeniach.</li>
+                   <li>Możemy zaplanować czas wolny od technologii, aby się zrelaksować i odprężyć.</li>
+             </ul>
+             <iframe src="https://www.meteoblue.com/pl/pogoda/widget/daily/ip_rumunia_675514?geoloc=fixed&days=7&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&precipunit=MILLIMETER&coloured=coloured&pictoicon=0&pictoicon=1&maxtemperature=0&maxtemperature=1&mintemperature=0&mintemperature=1&windspeed=0&windspeed=1&windgust=0&windgust=1&winddirection=0&winddirection=1&uv=0&uv=1&humidity=0&humidity=1&precipitation=0&precipitation=1&precipitationprobability=0&precipitationprobability=1&spot=0&spot=1&pressure=0&pressure=1&layout=light"  frameborder="0" scrolling="NO" allowtransparency="true" sandbox="allow-same-origin allow-scripts allow-popups allow-popups-to-escape-sandbox" style="width: 378px; height: 467px"></iframe><div><!-- DO NOT REMOVE THIS LINK --><a href="https://www.meteoblue.com/pl/pogoda/tydzie%C5%84/ip_rumunia_675514?utm_source=weather_widget&utm_medium=linkus&utm_content=daily&utm_campaign=Weather%2BWidget" target="_blank" rel="noopener">może pójdziesz na pole???</a></div>
+</body>
+</html>
